8df2579cbd41ff2ca7800aa9b15e7a167cad9fb0
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2024-12-20 23:45:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://admin:admin@admin.how18.mongodb.net/?retryWrites=true&w=majority&appName=admin")
-DATABASE_NAME = environ.get('DATABASE_NAME', "admin)
+DATABASE_NAME = environ.get('DATABASE_NAME', "admin")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
